775300ed9e7434913308e56bbce464420c80575e
IAPigarev <iapigarev@gmail.com>
2022-11-29 23:12:58 +0300

    add Example 008_dog project исправлена

--- Example008_dog/Program.cs
+++ Example008_dog/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-double distanse  =10000;
+﻿double time = 0, distanse  =1000;
 int count = 1,
 friend =2,
 firstFriendSpeed = 1,
@@ -11,17 +9,15 @@ while(distanse > 10)
 {
     if (friend==1)
     {
-        double time = distanse/ (dogSpeed-firstFriendSpeed);
+        time = distanse/ (dogSpeed-firstFriendSpeed);
         friend = 2;
     }
     else
     {
-        double time = distanse/ (dogSpeed+secondFriendSpeed);
+        time = distanse/ (dogSpeed+secondFriendSpeed);
         friend = 1;
     }
-    distanse = distanse - (secondFriendSpeed - firstFriendSpeed) * time;
+    distanse = distanse - ((secondFriendSpeed - firstFriendSpeed) *  time);
     count++;
 }
-Console.Write("Собака, чуть, не померла. Кругов");
-Console.Write(count);
-Console.WriteLine("пробежала.");
+Console.Write("Собака, чуть, не померла. Кругов "+count+" проебежала.");
