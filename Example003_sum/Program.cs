4b59f0c4c118094750c502af563c1f99bf01bda3
IAPigarev <iapigarev@gmail.com>
2022-11-29 17:57:26 +0300

    add  random in Example 003  project

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int namberA = 3;
-int namberB = 7;
+int namberA = new Random().Next(1,20);// cслучайное число от 1 до 19
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1,20);// cслучайное число от 1 до 19
+Console.WriteLine(namberB);
 int result = namberA + namberB; 
 Console.WriteLine(result);
