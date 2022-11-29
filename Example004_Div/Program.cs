cacabb93442f86acfb452b6343bb3cdc04d98e29
IAPigarev <iapigarev@gmail.com>
2022-11-29 17:42:08 +0300

    add Example 004 Div project

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double namberA = 14;
+double namberB = 3;
+double result = namberA / namberB; 
+Console.WriteLine(result);
