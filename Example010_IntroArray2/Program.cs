f406f6de91418d6836f18b919a1c555cff29b159
IAPigarev <iapigarev@gmail.com>
2022-12-02 16:19:41 +0300

    add Пример с работой массивами 2

---
+++ Example010_IntroArray2/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array ={12,23,13,24,52,63,74,38,23};
+int n =array.Length;// В переменную п записываем размер массива.
+int find = 23 ;
+
+int index = 0;
+while (index<n)
+{
+    if (array[index] == find) Console.WriteLine("Порядковый номер:"+index);
+    index++;//index=index+1;
+}    
